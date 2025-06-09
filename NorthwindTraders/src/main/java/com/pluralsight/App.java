b28d26d42002764953380ef44f75c16ba65be14b
Brian Miller <Bmiller995@gmail.com>
2025-06-09 14:16:37 -0500

    completed exercise

---
+++ NorthwindTraders/src/main/java/com/pluralsight/App.java
@@ -0,0 +1,40 @@
+package com.pluralsight;
+
+import java.sql.*;
+
+public class App {
+    public static void main(String[] args) {
+
+        try {
+
+            // 1. open a connection to the database
+            // use the database URL to point to the correct database
+            Connection connection;
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/northwind",
+                    "root",
+                    "yearup");
+
+            // create statement
+            // the statement is tied to the open connection
+            Statement statement = connection.createStatement();
+
+            // define your query
+            String query = "SELECT ProductName FROM products ";
+
+            // 2. Execute your query
+            ResultSet results = statement.executeQuery(query);
+
+            // process the results
+            while (results.next()) {
+                String city = results.getString("ProductName");
+                System.out.println(city);
+            }
+            // 3. Close the connection
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
